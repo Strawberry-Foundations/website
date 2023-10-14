6b01adcd097878af1dbfa8055ad472b1b6c9eb40
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-14 16:11:00 +0200

    added lang system

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, app, render_template
+from flask import Flask, render_template, request
 from init import *
+
 
 app = Flask(__name__, static_url_path=static_url_path, static_folder=static_folder, template_folder=template_folder)
 
 app.config['SECRET_KEY'] = secret
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+@app.route("/", defaults={'lang': 'en'})
+@app.route('/<string:lang>')
+def index(lang):        
+    def StringLoader(str):
+        return strloader(lang, str)
+        
+    return render_template('index.html', loader=StringLoader)
 
 
 if __name__ == "__main__":
