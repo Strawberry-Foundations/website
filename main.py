2a9fed22604f3ab93660a45fc8be51eaf635068e
Juliandev02 <juli.mackenstein@gmail.com>
2023-10-14 14:14:25 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, app, render_template
+from init import *
+
+
+app = Flask(__name__, static_url_path=static_url_path, static_folder=static_folder, template_folder=template_folder)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host=ip_address, port=port, debug=debug_mode, threaded=threaded)
